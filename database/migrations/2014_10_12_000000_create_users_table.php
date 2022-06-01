b8c1ca4723ab89055e773a0ae978e92804e7de7e
Firgisotya <zzuuddiinn28@gmail.com>
2022-06-01 19:51:43 +0700

    edit tabel migrasi user

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,6 +16,12 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('username');
+            $table->string('phone');
+            $table->string('gender');
+            $table->text('address');
+            $table->string('image');
+            $table->string('level');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
