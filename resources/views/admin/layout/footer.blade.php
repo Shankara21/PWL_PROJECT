d5505c95833785ff841bbcb7ce55d6798dae1804
Firgisotya <zzuuddiinn28@gmail.com>
2022-06-01 19:56:35 +0700

    menambahkan layout dashboard admin

---
+++ resources/views/admin/layout/footer.blade.php
@@ -0,0 +1,11 @@
+<!--**********************************
+            Footer start
+        ***********************************-->
+        <div class="footer">
+            <div class="copyright">
+                <p>Copyright © Designed &amp; Developed by <a href="../index.htm" target="_blank">DexignLab</a> 2021</p>
+            </div>
+        </div>
+        <!--**********************************
+            Footer end
+        ***********************************-->
