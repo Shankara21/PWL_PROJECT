a51bbeef66d79353fa14fb9df7fde7c758f39e93
Shankara21 <lazuardit21@gmail.com>
2022-07-18 19:47:21 +0700

    update navbar

--- resources/views/homepage/layouts/navbar.blade.php
+++ resources/views/homepage/layouts/navbar.blade.php
@@ -24,124 +24,116 @@
                                 class="fas fa-envelope"></i> Contact</a>
                     </div>
                 </div>
-                @guest
-                @if (Route::has('login'))
-                <a href="{{ route('login') }}" class="btn btn-secondary text-light rounded-pill py-2 px-4 ms-3">Sign
-                    In</a>
-                <a href="{{ route('register') }}" class="btn btn-light text-dark rounded-pill py-2 px-4 ms-3">Sign
-                    Up</a>
-                @endif
-                @else
-                @php
-                $order = \App\Models\Order::where('user_id', Auth::user()->id) -> where('status', 0) -> first();
-                // dd($order);
-                if ($order) {
-                $notifications = \App\Models\OrderDetail::where('order_id', $order -> id)->count();
-                }else{
-                $notifications = '';
-                }
-                @endphp
-                @if (Auth::user()->level == 'admin')
-                <div class="nav-item dropdown">
-                    <a href="/cart" class="nav-item nav-link"><i class="fas fa-shopping-cart"
-                            style="font-size: 1.5em"></i>
-                        {{-- @if ($orderDetails != null) --}}
-                        @if ($notifications)
-                        <span class="badge bg-danger"
-                            style="transform: translateY(-20px);padding: 3px 6px;border-radius: 30px">
-                            {{ $notifications }}
-                        </span>
-                        @else
-                        <span></span>
-                        @endif
-                        {{-- @else
-                        @endif --}}
-                    </a>
-                </div>
-                <div class="dropdown text-end navbar-nav">
-                    <a href="#" class="d-block link-dark text-decoration-none dropdown nav-item nav-link"
-                        id="dropdownUser1" data-bs-toggle="dropdown" aria-expanded="false">
-                        <span>{{Auth::user()->username}}</span>
-                        {{-- <img src="https://github.com/mdo.png" alt="mdo" width="32" height="32" class="rounded-circle"> --}}
-                    </a>
-                    <div class="dropdown-menu " style="transform: translateX(-20px)">
-                        <a href="/profile" class="dropdown-item"><i class="fas fa-user"></i> Profile </a>
-                        <a href="/dashboard" class="dropdown-item"><i class="fas fa-home"></i>Dashboard </a>
-                        <a class="dropdown-item" href="{{ route('logout') }}"
-                            onclick="event.preventDefault();
-                                                                                            document.getElementById('logout-form').submit();"><i
+
+            </div>
+            @guest
+            @if (Route::has('login'))
+            <a href="{{ route('login') }}" class="btn btn-secondary text-light rounded-pill py-2 px-4 ms-3">Sign
+                In</a>
+            <a href="{{ route('register') }}" class="btn btn-light text-dark rounded-pill py-2 px-4 ms-3">Sign
+                Up</a>
+            @endif
+            @else
+            @php
+            $order = \App\Models\Order::where('user_id', Auth::user()->id) -> where('status', 0) -> first();
+            // dd($order);
+            if ($order) {
+            $notifications = \App\Models\OrderDetail::where('order_id', $order -> id)->count();
+            }else{
+            $notifications = '';
+            }
+            @endphp
+            @if (Auth::user()->level == 'admin')
+            <div class="navbar-nav">
+                <a href="/cart" class="nav-item nav-link"><i class="fas fa-shopping-cart" style="font-size: 1.5em"></i>
+                    {{-- @if ($orderDetails != null) --}}
+                    @if ($notifications)
+                    <span class="badge bg-danger"
+                        style="transform: translateY(-20px);padding: 3px 6px;border-radius: 30px">
+                        {{ $notifications }}
+                    </span>
+                    @else
+                    <span></span>
+                    @endif
+                    {{-- @else
+                    @endif --}}
+                </a>
+            </div>
+            <div class="dropdown text-end navbar-nav">
+                <a href="#" class="d-block link-dark text-decoration-none dropdown nav-item nav-link" id="dropdownUser1"
+                    data-bs-toggle="dropdown" aria-expanded="false">
+                    <span>{{Auth::user()->username}}</span>
+                    {{-- <img src="https://github.com/mdo.png" alt="mdo" width="32" height="32" class="rounded-circle"> --}}
+                </a>
+                <ul class="dropdown-menu text-small" aria-labelledby="dropdownUser1"
+                    style="transform: translateX(-3em)">
+                    <li><a class="dropdown-item" href="/profile"><i class="fas fa-home"></i> Profile</a>
+                    <li><a class="dropdown-item" href="/dashboard"><i class="fas fa-home"></i> Dashboard</a>
+                    </li>
+                    <li>
+                        <hr class="dropdown-divider">
+                    </li>
+                    <li>
+                        <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();
+                                                                document.getElementById('logout-form').submit();"><i
                                 class="fas fa-sign-out-alt"></i>
-                            {{ __('Logout ') }}
+                            {{ __('Logout') }}
                             <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
                                 @csrf
                             </form>
                         </a>
-                        </li>
-                    </div>
-
-                    {{-- <ul class="dropdown-menu text-small" aria-labelledby="dropdownUser1"
-                        style="transform: translateX(-3em)">
-                        <li><a class="dropdown-item" href="/profile"><i class="fas fa-home"></i> Profile</a>
-                        <li><a class="dropdown-item" href="/dashboard"><i class="fas fa-home"></i> Dashboard</a>
-                        </li>
-                        <li>
-                            <hr class="dropdown-divider">
-                        </li>
-                        <li>
-                            <a class="dropdown-item" href="{{ route('logout') }}"
-                    onclick="event.preventDefault();
-                    document.getElementById('logout-form').submit();"><i class="fas fa-sign-out-alt"></i>
-                    {{ __('Logout') }}
-                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                        @csrf
-                    </form>
-                    </a>
                     </li>
-                    </ul> --}}
-                </div>
-                @endif
-                @if (Auth::user()->level == 'user')
-                <div class="navbar-nav">
-                    <a href="/cart" class="nav-item nav-link"><i class="fas fa-shopping-cart"
-                            style="font-size: 1.5em"></i>
-                        @if (!empty($orderDetails))
-                        <span class="badge bg-danger"
-                            style="transform: translateY(-20px);padding: 3px 6px;border-radius: 30px">
-                            {{ $notifications }}
-                        </span>
-                        @else
-                        @endif
-                    </a>
-                </div>
-                <div class="dropdown text-end navbar-nav">
-                    <a href="#" class="d-block link-dark text-decoration-none dropdown-toggle nav-item nav-link"
-                        id="dropdownUser1" data-bs-toggle="dropdown" aria-expanded="false">
-                        <span>{{Auth::user()->name}}</span>
-                        {{-- <img src="https://github.com/mdo.png" alt="mdo" width="32" height="32" class="rounded-circle"> --}}
-                    </a>
-                    <ul class="dropdown-menu text-small" style="transform: translateX(-3em)"
-                        aria-labelledby="dropdownUser1">
-                        <li><a class="dropdown-item" href="/Profile"><i class="fas fa-home"></i> Profile</a>
-                        </li>
-                        <li>
-                            <hr class="dropdown-divider">
-                        </li>
-                        <li>
-                            <a class="dropdown-item" href="{{ route('logout') }}"
-                                onclick="event.preventDefault();
-                                                                                    document.getElementById('logout-form').submit();"><i class="fas fa-sign-out-alt"></i>
-                                {{ __('Logout') }}
-                                <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                                    @csrf
-                                </form>
-                            </a>
-
-                        </li>
-                    </ul>
-                </div>
-                @endif
-                @endguest
+                </ul>
             </div>
+            @endif
+
+
+            @if (Auth::user()->level == 'user')
+            <div class="navbar-nav">
+                <a href="/cart" class="nav-item nav-link"><i class="fas fa-shopping-cart" style="font-size: 1.5em"></i>
+                    @if (!empty($orderDetails))
+                    <span class="badge bg-danger"
+                        style="transform: translateY(-20px);padding: 3px 6px;border-radius: 30px">
+                        {{ $notifications }}
+                    </span>
+                    @else
+                    @endif
+                </a>
+            </div>
+            <div class="dropdown text-end navbar-nav">
+                <a href="#" class="d-block link-dark text-decoration-none dropdown-toggle nav-item nav-link"
+                    id="dropdownUser1" data-bs-toggle="dropdown" aria-expanded="false">
+                    <span>{{Auth::user()->name}}</span>
+                    {{-- <img src="https://github.com/mdo.png" alt="mdo" width="32" height="32" class="rounded-circle"> --}}
+                </a>
+                <ul class="dropdown-menu text-small" style="transform: translateX(-3em)"
+                    aria-labelledby="dropdownUser1">
+                    <li><a class="dropdown-item" href="/Profile"><i class="fas fa-home"></i> Profile</a>
+                    </li>
+                    <li>
+                        <hr class="dropdown-divider">
+                    </li>
+                    <li>
+                        <a class="dropdown-item" href="{{ route('logout') }}"
+                            onclick="event.preventDefault();
+                                                                                document.getElementById('logout-form').submit();"><i class="fas fa-sign-out-alt"></i>
+                            {{ __('Logout') }}
+                            <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                @csrf
+                            </form>
+                        </a>
+
+                    </li>
+                </ul>
+            </div>
+            @endif
+
+            @endguest
+
+
+
         </div>
     </nav>
+
+
 </div>
